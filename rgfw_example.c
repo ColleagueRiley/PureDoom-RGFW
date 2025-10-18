f2f190234bce42898a2296b6c8501142d4dd05f6
ColleagueRiley <ColleagueRiley@gmail.com>
2025-10-17 23:53:01 -0400

    update RGFW

--- rgfw_example.c
+++ rgfw_example.c
@@ -19,12 +19,12 @@
 #include "mmeapi.h"
 #endif
 
-#define DOOM_IMPLEMENT_PRINT 
-#define DOOM_IMPLEMENT_FILE_IO 
-#define DOOM_IMPLEMENT_MALLOC 
-#define DOOM_IMPLEMENT_GETTIME 
-#define DOOM_IMPLEMENT_EXIT 
-#define DOOM_IMPLEMENT_GETENV 
+#define DOOM_IMPLEMENT_PRINT
+#define DOOM_IMPLEMENT_FILE_IO
+#define DOOM_IMPLEMENT_MALLOC
+#define DOOM_IMPLEMENT_GETTIME
+#define DOOM_IMPLEMENT_EXIT
+#define DOOM_IMPLEMENT_GETENV
 #include "PureDOOM.h"
 
 #include "stb_image_resize2.h"
@@ -40,7 +40,7 @@ doom_key_t RGFWScancode2DOOM[RGFW_keyLast] = {
     [RGFW_escape] = DOOM_KEY_ESCAPE,
     [RGFW_space] = DOOM_KEY_CTRL,
     [RGFW_apostrophe] = DOOM_KEY_APOSTROPHE,
-    [RGFW_multiply] = DOOM_KEY_MULTIPLY,
+    [RGFW_kpMultiply] = DOOM_KEY_MULTIPLY,
     [RGFW_comma] = DOOM_KEY_COMMA,
     [RGFW_minus] = DOOM_KEY_MINUS,
     [RGFW_period] = DOOM_KEY_PERIOD,
@@ -93,7 +93,7 @@ doom_key_t RGFWScancode2DOOM[RGFW_keyLast] = {
     [RGFW_left] = DOOM_KEY_LEFT_ARROW,
     [RGFW_up] = DOOM_KEY_UP_ARROW,
     [RGFW_right] = DOOM_KEY_RIGHT_ARROW,
-    [RGFW_down] = DOOM_KEY_DOWN_ARROW,      
+    [RGFW_down] = DOOM_KEY_DOWN_ARROW,
     [RGFW_shiftL] = DOOM_KEY_SHIFT,
     [RGFW_shiftR] = DOOM_KEY_SHIFT,
     [RGFW_F1] = DOOM_KEY_F1,
@@ -110,79 +110,11 @@ doom_key_t RGFWScancode2DOOM[RGFW_keyLast] = {
     [RGFW_F12] = DOOM_KEY_F12,
 };
 
-doom_key_t RGFWGamepad2DOOM[20] = {
-    [RGFW_gamepadY] = DOOM_KEY_TAB,
-    [RGFW_gamepadSelect] = DOOM_KEY_ENTER,
-    [RGFW_gamepadStart] = DOOM_KEY_ESCAPE,
-    [RGFW_gamepadX] = DOOM_KEY_CTRL,
-    [RGFW_gamepadA] = DOOM_KEY_E,
-    [RGFW_gamepadR2] = DOOM_KEY_CTRL,
-    [RGFW_gamepadLeft] = DOOM_KEY_A,
-    [RGFW_gamepadRight] = DOOM_KEY_D,
-    [RGFW_gamepadUp] = DOOM_KEY_UP_ARROW,
-    [RGFW_gamepadDown] = DOOM_KEY_DOWN_ARROW,      
-    [RGFW_gamepadL3] = DOOM_KEY_SHIFT,
-    
-    [RGFW_gamepadHome] = DOOM_KEY_UNKNOWN,
-    [RGFW_gamepadB] = DOOM_KEY_UNKNOWN,
-    [RGFW_gamepadL2] = DOOM_KEY_UNKNOWN,
-    [RGFW_gamepadR3] = DOOM_KEY_UNKNOWN,
-    [RGFW_gamepadL1] = DOOM_KEY_UNKNOWN,
-    [RGFW_gamepadR1] = DOOM_KEY_UNKNOWN,
-};
-
-void gamepad_update(u32 button, u32 press) {
-    u32 Dbutton = RGFWGamepad2DOOM[button];
-    u32 Dbutton2 = DOOM_KEY_UNKNOWN;
-
-    static char weapon = DOOM_KEY_1;
-    if (button == RGFW_gamepadR1 && weapon < DOOM_KEY_7) {
-        if (press == 0) weapon++;
-        press = !press;
-        Dbutton2 = weapon;
-    }
-    else if (button == RGFW_gamepadL1 && weapon > DOOM_KEY_1 && press == 0) {
-        if (press == 0) weapon--;
-        press = !press;
-        Dbutton2 = weapon;
-    }
-
-    switch (Dbutton) {
-        case DOOM_KEY_E: Dbutton2 = DOOM_KEY_ENTER; break;
-        case DOOM_KEY_UP_ARROW: Dbutton2 = DOOM_KEY_W; break;
-        case DOOM_KEY_DOWN_ARROW: Dbutton2 = DOOM_KEY_S; break;
-        default: break;
-    }
-    
-    if (press) {
-        doom_key_down(Dbutton);
-        doom_key_down(Dbutton2);
-    } else {
-        doom_key_up(Dbutton);
-        doom_key_up(Dbutton2);
-    }
-}
-
 doom_button_t RGFWButton2DOOM[4] = {
     [RGFW_mouseLeft] = DOOM_LEFT_BUTTON,
     [RGFW_mouseRight] = DOOM_RIGHT_BUTTON,
     [RGFW_mouseMiddle] = DOOM_MIDDLE_BUTTON,
 };
-
-/* this can also be used to convert BGR to RGB */
-void bitmap_rgbToBgr(u8* bitmap, RGFW_area a) {
-    /* loop through eacfh *pixel* (not channel) of the buffer */
-    u32 x, y;
-    for (y = 0; y < a.h; y++) {
-        for (x = 0; x < a.w; x++) {
-            u32 index = (y * 4 * a.w) + x * 4;
-
-            u8 red = bitmap[index];
-            bitmap[index] = bitmap[index + 2];
-            bitmap[index + 2] = red;
-        }
-    }    
-}
 
 #if defined(WIN32)
 static HMIDIOUT midi_out_handle = 0;
@@ -199,7 +131,7 @@ void send_midi_msg(uint32_t midi_msg)
 {
     if (audio_unit)
     {
-        MusicDeviceMIDIEvent(audio_unit, 
+        MusicDeviceMIDIEvent(audio_unit,
             (midi_msg) & 0xFF,
             (midi_msg >> 8) & 0xFF,
             (midi_msg >> 16) & 0xFF,
@@ -241,7 +173,7 @@ u32 tick_midi(u32 interval, void *param)
 }
 
 ma_bool8 g_isPaused = MA_TRUE;
-void audio_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount) {   
+void audio_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount) {
     if (g_isPaused)
         return;
 
@@ -249,29 +181,68 @@ void audio_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_ui
     memcpy(pOutput, buffer, frameCount * pDevice->playback.channels * sizeof(ma_uint16));
 }
 
+#ifdef _WIN32
+#include <windows.h>
+#elif __APPLE__
+#include <mach/mach_time.h>
+#else
+#include <time.h>
+#include <sys/time.h>
+#endif
+
+#ifndef _WIN32
+#include <pthread.h>
+#endif
+
+u64 getTimeNS(void) {
+#ifdef _WIN32
+    LARGE_INTEGER frequency, counter;
+    QueryPerformanceFrequency(&frequency);
+    QueryPerformanceCounter(&counter);
+    return (u64)((counter.QuadPart * 1000000000LL) / frequency.QuadPart);
+#elif __APPLE__
+    static mach_timebase_info_data_t timebase = {0};
+    if (timebase.denom == 0) {
+        mach_timebase_info(&timebase);
+    }
+    return (ticks * timebase.numer) / timebase.denom;
+#else
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return (u64)(ts.tv_sec * 1000000000 + ts.tv_nsec);
+#endif
+}
+
+void createThread(RGFW_proc proc, void* args) {
+#ifdef _WIN32
+    HANDLE thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)proc, args, 0, NULL);
+#else
+    pthread_t thread;
+    pthread_create(&thread, NULL, (void*(*)(void*))proc, args);
+#endif
+}
 
 void* thread(void* args) {
     static u64 midi_time = 0;
     static u64 lastTime = 0;
 
     while (1) {
-        if (RGFW_getTimeNS() - lastTime >= midi_time) {
+        if (getTimeNS() - lastTime >= midi_time) {
             midi_time = tick_midi(0, 0);
-            lastTime = RGFW_getTimeNS();
+            lastTime = getTimeNS();
         }
     }
 }
 
 int main(int argc, char** args) {
-#ifdef __EMSCRIPTEN__
-    RGFW_window* window = RGFW_createWindow("RGFW DOOM", RGFW_RECT(0, 0, 620, 400), RGFW_windowCenter | RGFW_windowAllowDND);  
-#else
-    RGFW_window* window = RGFW_createWindow("RGFW DOOM", RGFW_RECT(0, 0, 320, 200), RGFW_windowCenter);
-#endif
+    RGFW_window* window = RGFW_createWindow("RGFW DOOM", 0, 0, 320, 200, RGFW_windowCenter);
 
-    RGFW_window_initBuffer(window);
-    RGFW_area screenSize = RGFW_getScreenSize();
-    size_t buffer_stride = screenSize.w * 4;
+	RGFW_monitor mon = RGFW_window_getMonitor(window);
+
+	u8* buffer = RGFW_ALLOC(mon.mode.w * mon.mode.h * 4);
+    RGFW_surface* surface = RGFW_createSurface(buffer, mon.mode.w, mon.mode.h, RGFW_formatRGBA8);
+
+    size_t buffer_stride = mon.mode.w * 4;
     size_t doom_stride = WIDTH * 4;
 
     ma_device_config config = ma_device_config_init(ma_device_type_playback);
@@ -291,7 +262,7 @@ int main(int argc, char** args) {
 
     // Capture mouse
     RGFW_window_showMouse(window, 0);
-    RGFW_window_mouseHold(window, RGFW_AREA(0, 0));
+    RGFW_window_holdMouse(window);
 
     //-----------------------------------------------------------------------
     // Setup DOOM
@@ -305,7 +276,7 @@ int main(int argc, char** args) {
     doom_set_default_int("key_use", DOOM_KEY_E);
     doom_set_default_int("key_fire", DOOM_KEY_CTRL);
     doom_set_default_int("mouse_move", 0); // Mouse will not move forward
-    
+
     // Setup resolution
     doom_set_resolution(WIDTH, HEIGHT);
 
@@ -347,115 +318,62 @@ int main(int argc, char** args) {
 
     int active_mouse = 1; // Dev allow us to take mouse out of window
 
-    RGFW_createThread((void*)thread, NULL);
+    createThread((void*)thread, NULL);
 
     STBIR_RESIZE resize;
 
-    stbir_resize_init(&resize, NULL, WIDTH, HEIGHT, doom_stride,  window->buffer, 0, 0, buffer_stride, 
+    stbir_resize_init(&resize, NULL, WIDTH, HEIGHT, doom_stride,  buffer, 0, 0, buffer_stride,
                             STBIR_RGBA_NO_AW, STBIR_TYPE_UINT8_SRGB);
-    
+
     resize.fast_alpha = 1;
     resize.horizontal_filter = STBIR_FILTER_BOX;
     resize.vertical_filter = STBIR_FILTER_BOX;
-	
-	u32 fps = 0;
-    double start = RGFW_getTime();
-    u32 frames = 0;
 
     i32 j = 0;
     while (!done) {
-        while (RGFW_window_checkEvent(window)) {
-            switch (window->event.type) {
+		RGFW_event event;
+        while (RGFW_window_checkEvent(window, &event)) {
+            switch (event.type) {
                 case RGFW_quit:
                     done = 1;
                     break;
 
                 case RGFW_keyPressed:
-                    /* 
+                    /*
 					 * I don't know a better way to do this without it causing
 					 * bugs or having to modify PureDOOM
 					*/
-					if (window->event.key == RGFW_end || (window->event.key == RGFW_shiftL && RGFW_isPressed(window, RGFW_escape)))
+					if (event.key.value == RGFW_end || (event.key.value == RGFW_shiftL && RGFW_window_isKeyDown(window, RGFW_escape)))
                     {
 						RGFW_window_showMouse(window, active_mouse);
                         if (active_mouse)
-                            RGFW_window_mouseUnhold(window);
+                            RGFW_window_unholdMouse(window);
                         else
-                            RGFW_window_mouseHold(window, RGFW_AREA(0, 0));
-                        
+                            RGFW_window_holdMouse(window);
+
                         active_mouse = !active_mouse;
                     }
 
-                    doom_key_down(RGFWScancode2DOOM[window->event.key]);
+                    doom_key_down(RGFWScancode2DOOM[event.key.value]);
                     break;
 
                 case RGFW_keyReleased:
-                    doom_key_up(RGFWScancode2DOOM[window->event.key]);
+                    doom_key_up(RGFWScancode2DOOM[event.key.value]);
                     break;
 
                 case RGFW_mouseButtonPressed:
-                    if (active_mouse) doom_button_down(RGFWButton2DOOM[window->event.button]);
+                    if (active_mouse) doom_button_down(RGFWButton2DOOM[event.button.value]);
                     break;
 
                 case RGFW_mouseButtonReleased:
-                    if (active_mouse) doom_button_up(RGFWButton2DOOM[window->event.button]);
+                    if (active_mouse) doom_button_up(RGFWButton2DOOM[event.button.value]);
                     break;
 
                 case RGFW_mousePosChanged:
                     if (active_mouse)
                     {
-						doom_mouse_move(window->event.vector.x * 10, 0);
+						doom_mouse_move(event.mouse.vecX * 10, 0);
 					}
-                    break;
-                case RGFW_gamepadButtonPressed:
-                    gamepad_update(window->event.button, 1);
-                    break;
-                case RGFW_gamepadButtonReleased:
-                    gamepad_update(window->event.button, 0);
-                    break;
-                case RGFW_gamepadAxisMove:
-                    // axis 12
-                    switch (window->event.whichAxis) {
-                        case 0:
-                            if (window->event.axis[0].x >= 50) {
-                                doom_key_down(DOOM_KEY_D);
-                            }
-                            else if (window->event.axis[0].x <= -50)
-                                doom_key_down(DOOM_KEY_A);
-                            else {
-                                doom_key_up(DOOM_KEY_A);
-                                doom_key_up(DOOM_KEY_D);
-                            }
-                            
-                            if (window->event.axis[0].y >= 50) {
-                                doom_key_down(DOOM_KEY_S);
-                                doom_key_up(DOOM_KEY_DOWN_ARROW);
-                            }
-                            else if (window->event.axis[0].y <= -50) {
-                                doom_key_down(DOOM_KEY_W);
-                                doom_key_up(DOOM_KEY_UP_ARROW);
-                            }
-                            else {
-                                doom_key_up(DOOM_KEY_W);
-                                doom_key_up(DOOM_KEY_S);
-                                doom_key_up(DOOM_KEY_UP_ARROW);
-                                doom_key_up(DOOM_KEY_DOWN_ARROW);
-                            }
-                            break;                        
-                        // axis 2
-                        case 1:
-                            if (window->event.axis[1].x >= 50) {
-                                doom_key_down(DOOM_KEY_RIGHT_ARROW);
-                            }
-                            else if (window->event.axis[1].x <= -50)
-                                doom_key_down(DOOM_KEY_LEFT_ARROW);
-                            else {
-                                doom_key_up(DOOM_KEY_LEFT_ARROW);
-                                doom_key_up(DOOM_KEY_RIGHT_ARROW);
-                            }
-                            break;
-                        default: break;
-                    }
                     break;
             }
             if (done) break;
@@ -464,21 +382,25 @@ int main(int argc, char** args) {
 
         doom_update();
 
+		i32 width, height;
+		RGFW_window_getSize(window, &width, &height);
+
         resize.input_pixels = doom_get_framebuffer(4);
-        resize.output_w = resize.output_subw= window->r.w;
-        resize.output_h = resize.output_subh = window->r.h; 
+        resize.output_w = resize.output_subw= width;
+        resize.output_h = resize.output_subh = height;
         stbir_resize_extended(&resize);
 
-        RGFW_window_swapBuffers(window);
-		fps = RGFW_checkFPS(start, frames, 0);
-        frames++;
+        RGFW_window_blitSurface(window, surface);
     }
-    
+
 #if defined(WIN32)
     if (midi_out_handle) midiOutClose(midi_out_handle);
 #elif defined(__linux__)
     snd_rawmidi_close(midi_out_handle);
 #endif
+
+    RGFW_surface_free(surface);
+	RGFW_FREE(buffer);
 
     ma_device_uninit(&device);
     RGFW_window_close(window);
